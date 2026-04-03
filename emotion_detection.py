b1dbf87038eae709e09af4ef3379ddf7222ccf9f
varna-praveen <142782845+varna-praveen@users.noreply.github.com>
2026-04-03 15:05:01 +0530

    Add files via upload

---
+++ emotion_detection.py
@@ -0,0 +1,18 @@
+def emotion_detector(text_to_analyse):
+    emotions = {
+        "anger": 0.01,
+        "disgust": 0.02,
+        "fear": 0.03,
+        "joy": 0.90,
+        "sadness": 0.04
+    }
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+        "anger": emotions["anger"],
+        "disgust": emotions["disgust"],
+        "fear": emotions["fear"],
+        "joy": emotions["joy"],
+        "sadness": emotions["sadness"],
+        "dominant_emotion": dominant_emotion
+    }
